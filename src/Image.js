ac1bc9bbf68fa639a45251fb44804cacd81a3e8b
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-08-06 22:26:07 -0400

    adding attr

--- src/Image.js
+++ src/Image.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
-
 class ImageUpload extends React.Component {
     constructor(props) {
       super(props);
       this.state = {
         file: '',
+        imagevalue:'',
       imagePreviewUrl: '',
-      image:'https://i.imgur.com/wAlFbE8.png'
+      image:'https://i.imgur.com/wAlFbE8.png',
+      imageStore: 
+      [   {    
+        image: this.props.image,
+     
+
+      }  
+    ],
     };
+  
     this.style = {
-      background:"https://i.imgur.com/Rl0gjuU.png",
       width: "280px",
       height: "300px"      
    };
@@ -21,34 +28,49 @@ class ImageUpload extends React.Component {
     handleSubmit(e) {
       e.preventDefault();
       // TODO: do something with -> this.state.file
+      this.setState({ 
+        file: e.target.value
+      })
+ 
+    
+    window.print(this);
+    console.log('there is it');
       console.log('handle uploading-', this.state.file);
     }
   
     handleImageChange(e) {
       e.preventDefault();
-  
       let reader = new FileReader();
+
       let file = e.target.files[0];
-      let image = e.target.value;
-      reader.onloadend = () => {
+      var obj = {
+       file : file,
+       image : this
+      };
+      let store = this.state.imageStore;
+      console.log('this image store has passed', store);
+      store.push(obj , file);
+     reader.onloadend = (e) => {
         this.setState({
-          image:image,
-          file: file,
+          imageStore:  store,
           imagePreviewUrl: reader.result
         });
+
       }
-  
+      console.log('this images reader store has passed', store, this);
       reader.readAsDataURL(file)
+
+    // 
     }
   
     render() {
-      let {imagePreviewUrl, image} = this.state;
-      let $imagePreview = true;
+      let {imagePreviewUrl} = this.state;
+      let $imagePreview = null;
       if (imagePreviewUrl) {
         $imagePreview = (
         <div className="previewText"> 
-        <div className="b">  
-        <img  className="a" style={this.style} src={imagePreviewUrl} /></div>
+        <div className="b" style={this.style} >  
+        <img  className="a" src={imagePreviewUrl} /></div>
      </div>);
       } else {
         $imagePreview = (<div className="previewText"> </div>);
@@ -59,19 +81,24 @@ class ImageUpload extends React.Component {
           <form onSubmit={(e)=>this.handleSubmit(e)}>
             <input className="fileInput" 
               type="file" 
-              onChange={(e)=>this.handleImageChange(e)} />
+              onChange={(e)=>this.handleImageChange(e)}  ref={ref=> this.fileInput = ref}/>
             <button className="submitButton" 
               type="submit" 
               onClick={(e)=>this.handleSubmit(e)}>Upload Image</button>
           </form>
+      
           <div className="imgPreview" >
+          <img src={ this.state.image }/>
            {$imagePreview}
           
           </div>
           <div className="bottomright" >
           August 6, 2018
           </div>
+          <body id="capture">  </body>
+
         </div>
+        
       )
     }
   }
