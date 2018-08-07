fb808a6cf261be39901268ef3b87cf5946e50392
aimeelynnramirez <aimeelynnramirez@gmail.com>
2018-08-07 12:21:59 -0400

    pushing out the images to fit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
-import logo from './BannerHeader.jpg';
+import banner from './BannerHeader.jpg';
 import './App.css';
-import TextList from './ImageList';
 import ImageUpload from './Image.js';
-class App extends Component {
+import image from './Insta-Banner.png';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textvalue:'',
+      textStore: [
+        {
+          text:'Comment here for more to see!'
+        }  
+      ],
+    
+      };
+    this.handleImageChange = this.handleImageChange.bind(this)
+    this.handleImageClick = this.handleImageClick.bind(this)
+    
+    }  
+
+    handleImageChange(e) {
+    this.setState({ 
+      textvalue: e.target.value
+    })
+    
+  }
+
+  handleImageClick(event) {
+    let obj = {
+      text: this.state.textvalue
+   
+    };
+    
+    let store = this.state.textStore;
+     //console.log('this image store has passed', store);
+    store.push(obj);
+   // console.log('this image store has passed', obj);
+
+    this.setState({ 
+      textStore:  store,
+
+    });
+
+  }
   render() {
+    const list = this.state.textStore;
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-header" alt="logo" />
+        <header className="header">
+          <img  className="App-title" src={banner} alt="logo" />
         </header>
-    
-      <ImageUpload/>
+   <ImageUpload></ImageUpload>
+      <div className= "text" >
+           
+      
 
-       
+           </div>
       </div>
     );
   }
